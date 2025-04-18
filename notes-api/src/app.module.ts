42981ffaad80aac335a37f0ab0ee76a2803830b1
FP\tgrochocki <tgrochocki@future-processing.com>
2025-04-18 14:41:04 +0200

    add dockerfile for node app

--- notes-api/src/app.module.ts
+++ notes-api/src/app.module.ts
@@ -6,7 +6,15 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LinksModule } from './links/links.module';
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0NotesNest`;
+// MongoDB Atlas - nestjs-links/nest.links
+// with data from .env file
+// const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0NotesNest`;
+
+// MongoDB Comppas - nestjs-links/nest.links
+// const uri = `mongodb://localhost/nest`;
+
+// MongoDB Docker container
+const uri = 'mongodb://mongodb:27017/your-database-name';
 
 @Module({
   imports: [MongooseModule.forRoot(uri), ConfigModule.forRoot(), LinksModule],
